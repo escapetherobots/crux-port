415908373ab569831f02a2e423a6e1729da0dffb
Zach Thompson <escapetherobots@gmail.com>
2016-09-25 01:39:51 +0000

    Add logo and update paperclip image sizes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,15 @@ Rails.application.configure do
   
   #devise addition per instructions
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  # # Paperclip and s3 config
+  # config.paperclip_defaults = {
+  #   :storage => :s3,
+  #   :s3_host_name => 's3-us-west-2.amazonaws.com',
+  #   :s3_credentials => {
+  #     :bucket => ENV['S3_BUCKET_NAME'],
+  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
 end
