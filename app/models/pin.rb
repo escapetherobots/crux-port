86537377d1157ef8f783a6f194102f3ac2b34c18
Zach Thompson <escapetherobots@gmail.com>
2016-09-25 02:18:34 +0000

    Update paperclip image sizes ratio

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, styles: { large: "960x572", medium: "378x378>", thumb: "184x184>" }
+  has_attached_file :image, styles: { large: "960x572", medium: "360x215>", thumb: "160x96>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
